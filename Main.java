da644be09308274e658fa961fac627d412739638
Tyler <tyler.le@edu.sait.ca>
2023-05-17 14:34:56 -0600

    datatypes

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 22;
+        double gpa = 3.75;
+        char firstinital = 'T';
+        char lastinital = 'L';
+        Boolean perfectattendence = false;
+        System.out.println(age);
+        System.out.println(gpa);
+        System.out.println(firstinital);
+        System.out.println(lastinital);
+        System.out.println(perfectattendence);
+    }
+}
